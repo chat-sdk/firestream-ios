b1d92092bad2e033205a1a7749581b1687f60774
Pepe Becker <mail@pepebecker.com>
2020-02-10 20:07:02 +0900

    Create FirebaseService

---
+++ FireStream/Classes/Firebase/Service/FirebaseService.swift
@@ -0,0 +1,13 @@
+//
+//  FirebaseService.swift
+//  FireStream
+//
+//  Created by Pepe Becker on 2/10/20.
+//
+
+public class FirebaseService {
+
+    public var core: FirebaseCoreHandler!
+    public var chat: FirebaseChatHandler!
+
+}
