6f9b3dc9272a59c96d1e41cecf369ba9040b5f6e
Pepe Becker <mail@pepebecker.com>
2020-02-10 20:08:36 +0900

    Create Ref

---
+++ FireStream/Classes/Firebase/Firestore/Ref.swift
@@ -0,0 +1,53 @@
+//
+//  Ref.swift
+//  FireStream
+//
+//  Created by Pepe Becker on 2/10/20.
+//
+
+import FirebaseFirestore
+
+public class Ref {
+
+    public static func collection(_ path: Path) throws -> CollectionReference {
+        let ref = referenceFromPath(path)
+        if ref.isKind(of: CollectionReference.self) {
+            return ref as! CollectionReference
+        } else {
+            throw FireStreamError("R.string.error_mismatched_col_reference")
+        }
+    }
+
+    public static func document(_ path: Path) throws -> DocumentReference {
+        let ref = referenceFromPath(path)
+        if ref.isKind(of: DocumentReference.self) {
+            return ref as! DocumentReference
+        } else {
+            throw FireStreamError("R.string.error_mismatched_doc_reference")
+        }
+    }
+
+    public static func referenceFromPath(_ path: Path) -> NSObject {
+        var ref = db().collection(path.first()) as NSObject
+
+        for i in 0..<path.size() {
+            guard let component = path.get(i) else {
+                continue
+            }
+
+            if ref.isKind(of: DocumentReference.self) {
+                ref = (ref as! DocumentReference).collection(component)
+            } else {
+                ref = (ref as! CollectionReference).document(component)
+            }
+        }
+        return ref
+    }
+
+    public static func db() -> Firestore {
+        // MARK: Check this
+        return Firestore.firestore()
+    }
+
+}
+
