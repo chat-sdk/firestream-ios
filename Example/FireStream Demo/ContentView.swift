28170cb85d0b50b760be336ddc07d17d956d45d3
Pepe Becker <mail@pepebecker.com>
2020-02-24 18:53:37 +0900

    Add FireStream Demo target to support testing using Firebase

---
+++ Example/FireStream Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FireStream Demo
+//
+//  Created by Pepe Becker on 2/24/20.
+//  Copyright © 2020 FireStream. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
